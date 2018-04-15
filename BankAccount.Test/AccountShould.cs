345c3689df135cc3e27f19f154923b57df82eda3
Jorge Sanchez <xurxosf@gmail.com>
2018-04-15 20:51:33 +0200

    create first test with initial balance

---
+++ BankAccount.Test/AccountShould.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BankAccount.Test
+{
+    public class AccountShould
+    {
+        [Fact]
+        public void have_zero_balance_first_time()
+        {
+            decimal expectedBalance = 0m;
+
+            Account account = new Account();
+
+            decimal currentBalance = account.Balance;
+
+            Assert.Equal(expectedBalance, currentBalance);
+        }
+    }
+}
